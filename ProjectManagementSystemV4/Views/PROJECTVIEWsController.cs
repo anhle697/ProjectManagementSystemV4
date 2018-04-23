be82acb4617c9503f40ff66ca1e82e3a466aa835
Pato <patricio.lamacchia@gmail.com>
2018-04-23 16:32:13 -0500

    Added two views
    For employees so they don't have access to editing projects/deliverables

---
+++ ProjectManagementSystemV4/Views/PROJECTVIEWsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagementSystemV4.Models;
+
+namespace ProjectManagementSystemV4.Views
+{
+    public class PROJECTVIEWsController : Controller
+    {
+        private ProjectManagementSystemEntities db = new ProjectManagementSystemEntities();
+
+        // GET: PROJECTVIEWs
+        public ActionResult Index()
+        {
+            var pROJECTs = db.PROJECTs.Include(p => p.DEPARTMENT).Include(p => p.EMPLOYEE);
+            return View(pROJECTs.ToList());
+        }
+
+        // GET: PROJECTVIEWs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROJECT pROJECT = db.PROJECTs.Find(id);
+            if (pROJECT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pROJECT);
+        }
+
+    
+    }
+}
