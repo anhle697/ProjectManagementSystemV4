69dadb4099b6cb4d5f8764960aee2c5d68499e35
anhle697 <32230670+anhle697@users.noreply.github.com>
2018-04-23 15:28:54 -0500

    Dropdownbox

--- ProjectManagementSystemV4/App_Start/BundleConfig.cs
+++ ProjectManagementSystemV4/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ProjectManagementSystemV4
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +25,8 @@ namespace ProjectManagementSystemV4
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-theme.css"));
         }
     }
 }
